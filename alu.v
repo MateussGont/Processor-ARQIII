f6128167c73531f7a1c47544ccd566550bc77515
MateussGont <mateussilvagontijo@gmail.com>
2023-12-16 20:01:50 -0300

    ALU and Program Counter

--- alu.v
+++ alu.v
@@ -1,20 +1,29 @@
+module ALU(input [31:0] data1,data2,input [3:0] aluoperation,output reg [31:0] result,output reg zero,lt,gt);
+  always@(aluoperation,data1,data2)
+  begin 
+	   case (aluoperation)
+      4'b0000 : result = data1 + data2; // ADD
+      4'b0001 : result = data1 - data2; // SUB
+      4'b0010 : result = data1 & data2; // AND
+      4'b0011 : result = data1 | data2; // OR
+      4'b0100 : result = data1 ^ data2; // XOR
+      4'b0101 : result = {31'b0,lt};//slt
+      // if you want to add new Alu instructions  add here
+      default : result = data1 + data2; // ADD
+    endcase
+    if(data1>data2)
+      begin
+       gt = 1'b1;
+       lt = 1'b0; 
+      end else if(data1<data2)
+      begin
+       gt = 1'b0;
+       lt = 1'b1;  
+      end
+      
+    if (result==32'd0) zero=1'b1;
+    else zero=1'b0;
+  end
 
-module ALU (
-    input [31:0] operand1,
-    input [31:0] operand2,
-    input [3:0] operation,
-    output reg [31:0] result
-);
-    always @(operand1, operand2, operation) begin
-        case (operation)
-            4'b0000: result = operand1 + operand2;  // add
-            4'b0001: result = operand1 & operand2;  // and
-            4'b0010: result = operand1 | operand2;  // or
-            4'b0011: result = ~(operand1 | operand2);  // nor
-            4'b0100: result = operand1 << operand2;  // sll
-            4'b0101: result = operand1 >> operand2;  // srl
-            4'b0110: result = operand1 * operand2;  // mul
-            default: result = 32'b0;
-        endcase
-    end
 endmodule
+
--- data_memory.v
+++
@@ -1,8 +0,0 @@
-
-module data_memory (input [31:0] address, write_data, input MemWrite, MemRead, output [31:0] read_data);
-    reg [31:0] memory [0:1023]; // Ajuste o tamanho conforme necessário
-    always @(posedge clk)
-        if (MemWrite)
-            memory[address] <= write_data;
-    assign read_data = (MemRead) ? memory[address] : 0;
-endmodule
